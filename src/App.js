dfec7ccc5802b7681cc063e11fd51fd0dfeb92d0
Ainan-code <ainanhassan123@gmail.com>
2023-02-05 18:14:02 +0300

    working on reusable cards with props

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './header'
 import Header from './header';
 import TopBanner from './topbanner';
 import Card from './cards';
+import P1 from './pic1.jpg'
+
 
 function App() {
   return (
     <div>
       <Header />
     <TopBanner />
-    <Card title = "fdhbfdffdd"  description = "wdsidjjjjjjcnncn"></Card>
+    <Card title = "fdhbfdffdd"  description = "wdsidjjjjjjcnncn" <img src="P1"/> </Card>
     <Card title = "fdhbfdffdd"  description = "wdsidjjjjjjcnncn"></Card>
     <Card title = "fdhbfdffdd"  description = "wdsidjjjjjjcnncn"></Card>
     </div>
