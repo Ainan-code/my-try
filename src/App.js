d923381019d6d18e4c02a545bed41051895db5a2
Ainan-code <ainanhassan123@gmail.com>
2023-06-11 18:42:06 +0300

    cards components made with prpps

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import './header'
 import Header from './header';
 import TopBanner from './topbanner';
 import Card from './cards';
-import P1 from './pic1.jpg'
+import pic1 from './pic1.jpg';
+import pic2 from './pic2.jpg';
+import pic3 from './pic3.jpg';
 
 
-function App() {
+function App(props) {
   return (
     <div>
       <Header />
     <TopBanner />
-    <Card title = "fdhbfdffdd"  description = "wdsidjjjjjjcnncn" <img src="P1"/> </Card>
-    <Card title = "fdhbfdffdd"  description = "wdsidjjjjjjcnncn"></Card>
-    <Card title = "fdhbfdffdd"  description = "wdsidjjjjjjcnncn"></Card>
+    <div className="row">
+     <Card name ="BootStrap" imgUrl={pic1} button ='See Details' btnId="green-btn"/>
+     <Card name ="Life Coach" imgUrl={pic2} button ='See Details' btnId="blue-btn"/>
+     <Card name ="Responsive Design" imgUrl={pic3} button ='See Details' btnId="red-btn"  />
+     <Card name ="Vacation" imgUrl={pic1} button ='See Details' btnId="yellow-btn" />
+     <Card name =" Luxury" imgUrl={pic2} button ='See Details' btnId="brown-btn" />
+     
+    </div>
+    
     </div>
    
   
--- src/cards.js
+++ src/cards.js
@@ -1,14 +1,17 @@
 
         function Card(props) {
             return (
-              <div className="row">
-                <img className="card-img-top" src={props.featureImage} alt={props.title} />
-                <div className="card-body">
-                  <h5 className="card-title">{props.title}</h5>
-                  <p className="card-text">{props.description}</p>
-                  <a href={props.link} className="btn btn-primary">Learn more</a>
-                </div>
-              </div>
+              <div class="col sp-3" id="green">
+              <img src={props.imgUrl} alt={props.alttext} />
+              <h3 class="green-clr">{props.name}</h3>
+              <p>
+                Morbi sagittis justo a velit placerat ullamcorper quis quis velit.
+                Sed convallis at risus ullamcorper auctor. Praesent quis velit
+                neque. Quisque semper porta nisi vitae suscipit. Duis lectus magna,
+                ornare ac scelerisque.
+              </p>
+              <button class="btn" id={props.btnId ? props.btnId : "black"}>{props.button? props.button: "Read More" }</button>
+            </div>
             );
         }
         
